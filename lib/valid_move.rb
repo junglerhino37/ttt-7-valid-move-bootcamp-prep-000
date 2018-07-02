11ce1a11f09b57911e8864e4d40b8582d6f201a9
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 20:16:36 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,4 @@
 # code your #valid_move? method here
-puts pos_to_integer(9)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move(board, pos)
@@ -12,3 +11,5 @@ end
 def pos_to_integer(pos)
   return pos.to_i-1
 end
+
+puts pos_to_integer(9)
