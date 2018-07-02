2a53f32ce8c1864c2e97a7c7bf798318808bed43
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 20:15:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 # code your #valid_move? method here
-
+pos_to_integer(9)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move(board, pos)
