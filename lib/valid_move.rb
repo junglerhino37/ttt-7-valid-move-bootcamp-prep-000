800bf4b53016df5a1205463dfd8afb26ab2f0439
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 20:21:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move?(board, pos)
    pos=pos_to_integer(pos)
-  return (pos.between?(0,8)&& position_taken?(board,pos))
+  return (pos.between?(0,8)&& !position_taken?(board,pos))
 end
 
 def position_taken?(board,pos)
